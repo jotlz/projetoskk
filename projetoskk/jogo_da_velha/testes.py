e922693fe6cecdc49a4e4606e357a7ea8b9a3a00
Jotz <barreirajoaolucas531@gmail.com>
2026-04-18 20:10:13 -0300

    Consegui fazer os turnos funcionarem de uma forma mais limpa e melhor de entender

--- projetoskk/jogo_da_velha/testes.py
+++ projetoskk/jogo_da_velha/testes.py
@@ -9,71 +9,95 @@ xis = '✖'
 ball = '⚉'
 
 print('[+] Jogo da Velha')
-print('\nJogador 1(J1): X\nJogador 2(J2): O')
+print(f'\nJogador 1(J1): {xis}\nJogador 2(J2): {ball}')
 
 print(f"\n {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
 
 #* função que chama o jogo
-def jogo():
-    #*placar de vitórias
-    winsj1 = 0
-    winsj2 = 0
+jogador_atual = xis
+while True:
+    # def jogo():
+        #*placar de vitórias
+        #// winsj1 = 0
+        #// winsj2 = 0
 
-    if winsj1 > 0 or winsj2 > 0:
-        print(f"\nPlacar: J1=>{winsj1} | J2=>{winsj2}")
-
-    #* laço que faz os turnos do jogo funcionarem
-    while True: 
-        #* função que permite o j1 jogar e caso repetir uma casa ocupada repete a jogada
-        def jogada1():
-            while True:
-                buscar_casa = False
-                j1 = int(input('J1 - Posição: '))
-                if j1 not in [1,2,3,4,5,6,7,8,9]:
-                    print("Entrada Inválida")
-                else:
-                    for i, tuplas in enumerate(matriz):
-                        for j, posicao in enumerate(tuplas):
-                            if isinstance(posicao, int):
-                                if posicao == j1:
-                                    linha = i
-                                    coluna = j
-                                    buscar_casa = True
-                                    break
-                        if buscar_casa == True:
-                            break
-
-                    if buscar_casa == True:
-                        matriz[linha][coluna] = xis
-                        print(f" {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
-                        return False
+        #// if winsj1 > 0 or winsj2 > 0:
+        #//     print(f"\nPlacar: J1=>{winsj1} | J2=>{winsj2}")
+        casa_ocupada = False
+        try:
+            posicao = int(input(f"{jogador_atual} - Posição: "))
+            if posicao not in [1,2,3,4,5,6,7,8,9]:
+                print("Entrada Inválida!")
+            else:
+                for i, tuplas in enumerate(matriz):
+                    for j, itens in enumerate(tuplas):
+                        if isinstance(itens, int):
+                            if itens == posicao:
+                                linha = i
+                                coluna = j
+                                casa_ocupada = True
+                                break
+                    if casa_ocupada == True:
+                        break
+                if casa_ocupada == True:
+                    matriz[linha][coluna] = jogador_atual
+                    print(f"\n {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
+                    if jogador_atual == xis:
+                        jogador_atual = ball
                     else:
-                        print('Casa Ocupada!\nTente novamente...')
-
-            #* função que permite o j2 jogar e caso repetir uma casa ocupada repete a jogada
-        def jogada2():
-            while True:
-                buscar_casa = False
-                j2 = int(input('J2 - Posição: '))
-                if j2 not in [1,2,3,4,5,6,7,8,9]:
-                    print('Entrada Inválida')
+                        jogador_atual = xis
                 else:
-                    for i, tuplas in enumerate(matriz):
-                        for j, posicao in enumerate(tuplas):
-                            if isinstance(posicao, int):
-                                if posicao == j2:
-                                    linha = i
-                                    coluna = j
-                                    buscar_casa = True
-                                    break
-                        if buscar_casa == True:
-                            break
-                    if buscar_casa == True:
-                        matriz[linha][coluna] = ball
-                        print(f" {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
-                        return False
-                    else:
-                        print('Casa Ocupada!\nTente Novamente...')
+                    print("Casa Ocupada!\nTente Outra Posição!")
+        except ValueError:
+            print("Entrada Inválida!\nA Posição deve ser dada apenas em números inteiros!!")
+    # jogo()
+        #//     while True:
+        #//         buscar_casa = False
+        #//         j1 = int(input('J1 - Posição: '))
+        #//         if j1 not in [1,2,3,4,5,6,7,8,9]:
+        #//             print("Entrada Inválida")
+        #//         else:
+        #//             for i, tuplas in enumerate(matriz):
+        #//                 for j, posicao in enumerate(tuplas):
+        #//                     if isinstance(posicao, int):
+        #//                         if posicao == j1:
+        #//                             linha = i
+        #//                             coluna = j
+        #//                             buscar_casa = True
+        #//                             break
+        #//                 if buscar_casa == True:
+        #//                     break
+
+        #//             if buscar_casa == True:
+        #//                 matriz[linha][coluna] = xis
+        #//                 print(f" {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
+        #//                 return False
+        #//             else:
+        #//                 print('Casa Ocupada!\nTente novamente...')
+
+        #//     #* função que permite o j2 jogar e caso repetir uma casa ocupada repete a jogada
+        #// def jogada2():
+        #//     while True:
+        #//         buscar_casa = False
+        #//         j2 = int(input('J2 - Posição: '))
+        #//         if j2 not in [1,2,3,4,5,6,7,8,9]:
+        #//             print('Entrada Inválida')
+        #//         else:
+        #//             for i, tuplas in enumerate(matriz):
+        #//                 for j, posicao in enumerate(tuplas):
+        #//                     if isinstance(posicao, int):
+        #//                         if posicao == j2:
+        #//                             linha = i
+        #//                             coluna = j
+        #//                             buscar_casa = True
+        #//                             break
+        #//                 if buscar_casa == True:
+        #//                     break
+        #//             if buscar_casa == True:
+        #//                 matriz[linha][coluna] = ball
+        #//                 print(f" {matriz[0][0]} | {matriz[0][1]} | {matriz[0][2]} \n---+---+---\n {matriz[1][0]} | {matriz[1][1]} | {matriz[1][2]} \n---+---+---\n {matriz[2][0]} | {matriz[2][1]} | {matriz[2][2]} \n")
+        #//                 return False
+        #//             else:
+        #//                 print('Casa Ocupada!\nTente Novamente...')
         #@ verifica se ganhou ou não
-        jogada1(),jogada2()
-jogo()
+#// jogo()
